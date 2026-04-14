2b239220056539eb946371268ecb119c7e75130a
lukeabram <luke@k2d2.org>
2026-04-13 22:55:39 -0500

    frequency

---
+++ lua/plugins/frequency.lua
@@ -0,0 +1,6 @@
+return {
+	{
+		"nvim-telescope/telescope-frecency.nvim",
+		dependencies = { "nvim-telescope/telescope.nvim" },
+	},
+}
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,15 +1,17 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    local telescope = require("telescope")
-    telescope.setup()
+	"nvim-telescope/telescope.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local telescope = require("telescope")
+		telescope.setup({ extensions = { frequency = {} } })
 
-    local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<leader>ff", builtin.find_files)
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep)
-     vim.keymap.set("n", "<leader>fh", builtin.oldfiles, { desc = "Recently opened files" })
-    vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "Frecency/MRU" })
-    vim.keymap.set("n", "<leader>fm", builtin.marks, { desc = "Jump to bookmarks" })
-end,
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>ff", builtin.find_files)
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep)
+		vim.keymap.set("n", "<leader>fh", builtin.oldfiles, { desc = "Recently opened files" })
+		vim.keymap.set("n", "<leader>fr", function()
+			telescope.extensions.frecency.frecency()
+		end, { desc = "Frecency/MRU" })
+		vim.keymap.set("n", "<leader>fm", builtin.marks, { desc = "Jump to bookmarks" })
+	end,
 }
